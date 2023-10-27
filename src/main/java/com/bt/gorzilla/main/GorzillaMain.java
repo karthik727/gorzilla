d04186e7e8755e8745c1e818c27c40ae74a77839
karthik <karthik727@gmail.com>
2023-10-27 17:34:18 +0530

    Gorzilla first commit. Changes: POM inclusion of spring boot jars Main class application property file

---
+++ src/main/java/com/bt/gorzilla/main/GorzillaMain.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.bt.gorzilla.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author govinda
+ *
+ */
+@SpringBootApplication
+public class GorzillaMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GorzillaMain.class, args);
+
+	}
+
+}
